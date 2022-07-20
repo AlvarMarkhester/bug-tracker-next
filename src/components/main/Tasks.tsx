6bb35ee47e5d1b84138ae0ce7f0a9d707de15431
Alvar Markhester <45149278+AlvarMarkhester@users.noreply.github.com>
2022-07-20 15:56:40 +0200

    Simplyfying

--- src/components/main/Tasks.tsx
+++ src/components/main/Tasks.tsx
@@ -13,36 +13,35 @@ import { Input } from "@chakra-ui/react";
 const Tasks = () => {
     return (
         <>
-        <Flex p={'10px'} w={'100%'}>
-            <Grid
-                w="100%"
-                templateRows="repeat(2, 1fr)"
-                templateColumns="repeat(5, 1fr)"
-                gap={4}
-            >
-                <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                    <Text align={"center"} bg="red.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Backlog</Text>
-                    <Button width="100%" height="20px">+</Button>
-                </GridItem>
-                <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                    <Text align={"center"} bg="yellow.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Todo</Text>
-                    <Button width="100%" height="20px">+</Button>
-                </GridItem>
-                <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                    <Text align={"center"} bg="teal.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>In progress</Text>
-                    <Button width="100%" height="20px">+</Button>
-                </GridItem>
-                <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                    <Text align={"center"} bg="green.200" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Code Review</Text>
-                    <Button width="100%" height="20px">+</Button>
-                </GridItem>
-                <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                    <Text align={"center"} bg="green.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Finished</Text>
-                    <Button width="100%" height="20px">+</Button>
-                </GridItem>
-            </Grid>
-        </Flex>
-        <AddNewTask/>
+            <Flex p={'10px'} w={'100%'}>
+                <Grid
+                    w="100%"
+                    templateRows="repeat(2, 1fr)"
+                    templateColumns="repeat(5, 1fr)"
+                    gap={4}
+                >
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="red.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Backlog</Text>
+                        <AddNewTask />
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="yellow.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Todo</Text>
+                        <AddNewTask />
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="teal.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>In progress</Text>
+                        <AddNewTask />
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="green.200" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Code Review</Text>
+                        <AddNewTask />
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="green.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Finished</Text>
+                        <AddNewTask />
+                    </GridItem>
+                </Grid>
+            </Flex>
         </>
     );
 };
@@ -50,7 +49,10 @@ const Tasks = () => {
 const AddNewTask = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     return (
+        <>
+        <Button width="100%" height="20px" onClick={onOpen}>+</Button>
         <NewTaskModal isOpen={isOpen} onClose={onClose} />
+        </>
     )
 }
 
