2a4553e24cb3c245e55909cc3c81808b8df4b9a4
Alvar Markhester <45149278+AlvarMarkhester@users.noreply.github.com>
2022-07-29 16:34:59 +0200

    Refactor tasks

--- src/components/main/tasks/NewTaskModal.tsx
+++ src/components/main/tasks/NewTaskModal.tsx
@@ -1,61 +1,15 @@
-import { Flex, Grid, GridItem, useColorModeValue, Text, Button, VStack, useDisclosure, ModalFooter, Textarea } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { Modal } from "@chakra-ui/react";
+import { Button, Flex, Modal } from "@chakra-ui/react";
 import { ModalContent } from "@chakra-ui/react";
 import { ModalHeader } from "@chakra-ui/react";
 import { ModalCloseButton } from "@chakra-ui/react";
 import { ModalBody } from "@chakra-ui/react";
 import { ModalOverlay } from "@chakra-ui/react";
 import { FormControl } from "@chakra-ui/react";
-import { FormLabel } from "@chakra-ui/react";
+import { FormLabel, Textarea, Text, VStack } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-
-
-const Tasks = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [modalType, setModalType] = useState("")
-    const openModal = (type: string) => {
-        setModalType(type)
-        onOpen()
-    }
-    return (
-        <>
-            <Flex p={'10px'} w={'100%'}>
-                <Grid
-                    w="100%"
-                    templateRows="repeat(2, 1fr)"
-                    templateColumns="repeat(5, 1fr)"
-                    gap={4}
-                >
-                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                        <Text align={"center"} bg="red.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Backlog</Text>
-                        <Button width="100%" height="20px" onClick={() => openModal("Backlog")}>+</Button>
-                    </GridItem>
-                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                        <Text align={"center"} bg="yellow.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Todo</Text>
-                        <Button width="100%" height="20px" onClick={() => openModal("Todo")}>+</Button>
-                    </GridItem>
-                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                        <Text align={"center"} bg="teal.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>In progress</Text>
-                        <Button width="100%" height="20px" onClick={() => openModal("Progress")}>+</Button>
-                    </GridItem>
-                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                        <Text align={"center"} bg="green.200" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Code Review</Text>
-                        <Button width="100%" height="20px" onClick={() => openModal("Review")}>+</Button>
-                    </GridItem>
-                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
-                        <Text align={"center"} bg="green.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Finished</Text>
-                        <Button width="100%" height="20px" onClick={() => openModal("Finished")}>+</Button>
-                    </GridItem>
-                </Grid>
-            </Flex>
-            <NewTaskModal isOpen={isOpen} onClose={onClose} type={modalType} />
-        </>
-    );
-};
 
 const NewTaskModal = ({ isOpen, onClose, type }: { isOpen: boolean, onClose: () => void, type: string }) => {
     const { data } = useSession();
@@ -119,5 +73,4 @@ const NewTaskModal = ({ isOpen, onClose, type }: { isOpen: boolean, onClose: () 
     );
 
 }
-
-export default Tasks;
+export default NewTaskModal;
---
+++ src/components/main/tasks/Tasks.tsx
@@ -0,0 +1,52 @@
+import { Flex, Grid, GridItem, useColorModeValue, Text, Button, VStack, useDisclosure, Textarea } from "@chakra-ui/react";
+import React, { useState } from "react";
+import NewTaskModal from "./NewTaskModal";
+
+
+
+const Tasks = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [modalType, setModalType] = useState("")
+    const openModal = (type: string) => {
+        setModalType(type)
+        onOpen()
+    }
+    return (
+        <>
+            <Flex p={'10px'} w={'100%'}>
+                <Grid
+                    w="100%"
+                    templateRows="repeat(2, 1fr)"
+                    templateColumns="repeat(5, 1fr)"
+                    gap={4}
+                >
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="red.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Backlog</Text>
+                        <Button width="100%" height="20px" onClick={() => openModal("Backlog")}>+</Button>
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="yellow.300" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Todo</Text>
+                        <Button width="100%" height="20px" onClick={() => openModal("Todo")}>+</Button>
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="teal.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>In progress</Text>
+                        <Button width="100%" height="20px" onClick={() => openModal("Progress")}>+</Button>
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="green.200" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Code Review</Text>
+                        <Button width="100%" height="20px" onClick={() => openModal("Review")}>+</Button>
+                    </GridItem>
+                    <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("white", "gray.700")} borderRadius={"10px"}>
+                        <Text align={"center"} bg="green.400" borderTopRadius={"10px"} borderBottomRadius={useColorModeValue("10px", undefined)}>Finished</Text>
+                        <Button width="100%" height="20px" onClick={() => openModal("Finished")}>+</Button>
+                    </GridItem>
+                </Grid>
+            </Flex>
+            <NewTaskModal isOpen={isOpen} onClose={onClose} type={modalType} />
+        </>
+    );
+};
+
+
+
+export default Tasks;
